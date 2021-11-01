76ad074a9319ea089565e0e86b5bcc0516a7df7f
stwgabriel <gabrielstw@pm.me>
2021-11-01 08:29:19 -0300

    feat: add Me section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,44 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { NavigationContext } from '../../contexts/NavigationContext';
 
-import { Content, MainView, MeView } from './styles';
+import {
+  Content, MainView, MeView,
+} from './styles';
 
 import { PageContainer } from '../../components/PageContainer';
+import { PageTitle } from '../../components/PageTitle';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import scroll from '../../assets/images/icons/mouse-scroll.svg';
+import mePicture from '../../assets/images/me-picture.png';
+import downloadArrow from '../../assets/images/icons/download-arrow.svg';
 
 function Home() {
 
   const navigation = useContext(NavigationContext);
   const { handleNavigation } = navigation;
 
-  function handleScroll() {
+  const [modalVisible, setModalVisible] = useState(false);
 
+  function handleScroll(e) {
+
+    e.preventDefault();
+    const { target } = e;
+    const finalTargetId = target.getAttribute('href');
+    const finalTarget = document.querySelector(finalTargetId).offsetTop;
+
+    document.getElementById('page-container').scroll({
+      top: finalTarget + 90,
+      behavior: 'smooth',
+    });
+  }
+  function handleModalVisibility() {
+
+    setModalVisible(() => (!modalVisible));
+
+    console.log(modalVisible);
   }
 
   useEffect(() => {
@@ -26,15 +48,16 @@ function Home() {
     intenalLinks.forEach((item) => { item.addEventListener('click', handleScroll); });
 
     return () => {
-      intenalLinks.forEach((item) => { item.removeEventListener('click', handleScroll); });
 
+      intenalLinks.forEach((item) => { item.removeEventListener('click', handleScroll); });
     };
   }, []);
 
   return (
-    <PageContainer>
+    <PageContainer id="page-container">
 
       <Content>
+
         <MainView>
 
           <div className="row">
@@ -51,43 +74,67 @@ function Home() {
 
           </div>
 
-          <button type="button" className="scroll-down">
+          <a href="#me" className="scroll-down no-select">
             <span>Scroll Down</span>
             <img src={scroll} alt="Scroll" />
-          </button>
+          </a>
 
         </MainView>
+
         <MeView id="me">
-          <span>random</span>
+
+          <PageTitle className="no-select"> Me </PageTitle>
+          <div className="me-content">
+
+            <div className="content__text">
+              <p>
+                Hi, I&lsquo;m Gabriel and I&lsquo;ll tell you a little bit about myself, I&lsquo;m
+                18 years old and I&lsquo;m a Front-End Developer located in Brazil, passionate about
+                web development, graphic arts, and helping! I&lsquo;m always interested in cool
+                stuff.
+              </p>
+              <p>
+                Currently self-taught in web development and design, I always try to keep myself
+                updated and organized in subjects of the area so I can always offer the best of
+                myself in my creations.
+              </p>
+              <p>
+                I am interested in the whole spectrum of front-end development and am
+                looking for a first opportunity to work on ambitious projects and hopefully
+                learn a lot from them.
+              </p>
+            </div>
+
+            <div className="content__picture no-select">
+              <img src={mePicture} alt="Me" className="no-select" />
+              <button onClick={handleModalVisibility} className="download-resume" type="button">
+                <span>Resume</span>
+                <img src={downloadArrow} alt="download" />
+              </button>
+            </div>
+
+          </div>
         </MeView>
 
-        <div className="spaces">
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-        </div>
+        {/* {modalVisible && (
+          <DownloadOverlay>
+            <div className="download-modal">
+              <div className="click-to-exit">
+                x
+              </div>
+              <ul className="options">
+                <li className="option">
+                  label
+                </li>
+              </ul>
+            </div>
+          </DownloadOverlay>
+        )} */}
 
       </Content>
 
       <Link
-        className="page-border from-right toPortifolio"
+        className="page-border from-right toPortifolio no-select"
         onClick={handleNavigation}
         to="/portfolio"
       >
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -22,6 +22,8 @@ const MainView = styled.section`
   min-height: 38rem;
   height: clamp(38rem, 85vh, 180rem);
 
+  padding-bottom:clamp(100px, 10vh, 10rem);
+
   .row {
 
     display: flex;
@@ -70,14 +72,19 @@ const MainView = styled.section`
 
     position: absolute;
     bottom: 2rem;
+    left: -1rem;
+
+    padding: 1rem;
 
     span {
 
       margin-right: 1.6rem;
+      pointer-events: none;
     }
 
     img {
 
+      pointer-events: none;
       width: 1.6rem;
     }
   }
@@ -85,6 +92,96 @@ const MainView = styled.section`
 
 const MeView = styled.section`
 
+  padding: clamp(100px, 10vh, 10rem) 0;
+
+  .me-content {
+
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+
+    margin-top: clamp(3.3rem, 6vh, 10rem);
+
+    .content__text {
+
+      font-size: clamp(1.6rem, 1.5vw, 2.8rem);
+      font-size: 600;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      padding-right: 12rem;
+
+      p {
+
+        margin: 1.5rem 0;
+      }
+    }
+
+    .content__picture {
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      flex:1;
+
+      .download-resume {
+
+        background: rgba(255, 255, 255, 0.15);
+        border: none;
+        backdrop-filter: blur(4px);
+        border-radius: .8rem;
+        color: ${({ theme }) => theme.color.white};
+        font-size: clamp(1.6rem, 1.2vw, 2.5rem);
+        font-weight: 600;
+
+        display: flex;
+        align-items:center;
+        justify-content: center;
+
+        padding: 1.2rem 2rem;
+        margin-top: 3.3rem;
+
+        transition: background .3s;
+
+        span {
+
+          margin-right: 2rem;
+        }
+
+        img {
+
+          width: clamp(1.6rem, 1.6vw, 2rem);
+        }
+
+        &:hover {
+
+          background: ${({ theme }) => theme.color.hotPurple};
+        }
+      }
+
+      img {
+
+        width: clamp(20rem, 30vw, 60rem);
+      }
+    }
+  }
 `;
 
-export { Content, MainView, MeView };
+// const DownloadOverlay = styled.div`
+
+//   background: #0005;
+
+//   position: fixed;
+//   top:0;
+//   bottom:0;
+//   /* left:0; */
+//   right:0;
+//   transform: translate(-50%, -50%)
+// `;
+
+export {
+  Content, MainView, MeView,
+};
