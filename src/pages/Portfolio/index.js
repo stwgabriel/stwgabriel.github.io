69cfa752de683a4184a15be933a12f703c525c9f
stwgabriel <gabrielstw@pm.me>
2022-01-02 19:11:55 -0300

    feat: add horizontal scroll, add borderSide prop...
    create HorizontalScrollView styled component
    
    create project styled component
    
    add borderPage prop
    
    import and add HorizontalScrollView
    
    import and add Projects on HorizontalScroll

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -6,6 +6,7 @@ import PageContainer from '../../components/PageContainer';
 import PageBorder from '../../components/PageBorder';
 import PageView from '../../components/PageView';
 import PageTitle from '../../components/PageTitle';
+import { HorizontalScrollView, Project } from './styles';
 
 function Portfolio() {
 
@@ -17,6 +18,7 @@ function Portfolio() {
     <PageContainer>
 
       <PageBorder
+        borderSide="left"
         handleNavigation={handleNavigation}
         from="from-left"
         to=""
@@ -26,9 +28,31 @@ function Portfolio() {
 
       <PageView>
         <PageTitle className="no-select">Portfolio</PageTitle>
+        <HorizontalScrollView>
+          <h2>Front-end Projects</h2>
+          <Project>
+            <div className="project__image">
+              {/* <img src="" alt="Project Preview" /> */}
+            </div>
+            <div className="project__content--overlay">
+              <div className="project__content">
+                <header>
+                  <h3>Title</h3>
+                  <button className="expand-close-info" type="button">{' '}</button>
+                </header>
+                <p className="content__description">description</p>
+                <h3>Tech</h3>
+                <div className="tech-tags">
+                  <span className="tech-tag">HTML</span>
+                </div>
+              </div>
+            </div>
+          </Project>
+        </HorizontalScrollView>
       </PageView>
 
       <PageBorder
+        borderSide="right"
         handleNavigation={handleNavigation}
         from="from-right"
         to="skills"
