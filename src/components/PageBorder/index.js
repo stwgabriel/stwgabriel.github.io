4f5a04d0b1eb404ff9a33915a48d1ba770e78190
stwgabriel <gabrielstw@pm.me>
2022-01-02 16:41:00 -0300

    feat: update imports, change component behavior
    rename arrow import to rightArrow add leftArrow import and remove import on styled component
    
    add borderSide prop to inform the components position
    
    reorder Link props
    
    remove pseudo component interation and add a hoverLayer component
    
    remove old class interation and add a dynamic intaration based on props

--- src/components/PageBorder/index.js
+++ src/components/PageBorder/index.js
@@ -1,30 +1,44 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import arrow from '../../assets/images/icons/arrow.svg';
+import rightArrow from '../../assets/images/icons/right-arrow.svg';
+import leftArrow from '../../assets/images/icons/left-arrow.svg';
 import { Container } from './styles';
 
 function PageBorder({
-  handleNavigation, from, to, children,
+  borderSide, handleNavigation, from, to, children,
 }) {
 
   return (
-    <Container>
+    <Container borderSide={borderSide}>
       <Link
-        className={`page-border ${from} to-${to === '' ? 'home' : to} no-select`}
-        onClick={handleNavigation}
         to={`/${to}`}
+        onClick={handleNavigation}
+        className={`page-border ${from} to-${to === '' ? 'home' : to} no-select`}
       >
-        <img className="arrow" src={arrow} alt="Arrow" />
-        <span>{children}</span>
+        <img
+          alt="Arrow"
+          className="arrow no-select no-events"
+          src={borderSide === 'left' ? leftArrow : rightArrow}
+        />
+        <span className="no-select no-events">{children}</span>
 
       </Link>
+      <div className="hoverLayer no-select no-events">
+        <img
+          alt="Arrow"
+          className="arrow"
+          src={borderSide === 'left' ? leftArrow : rightArrow}
+        />
+        <span>{children}</span>
+      </div>
     </Container>
   );
 }
 
 PageBorder.propTypes = {
 
+  borderSide: PropTypes.string.isRequired,
   handleNavigation: PropTypes.func.isRequired,
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
--- src/components/PageBorder/styles.js
+++ src/components/PageBorder/styles.js
@@ -1,21 +1,19 @@
 import styled from 'styled-components';
 
-import arrow from '../../assets/images/icons/arrow.svg';
-import leftArrow from '../../assets/images/icons/left-arrow.svg';
-
 const Container = styled.div`
 
   position: sticky;
-  top:0;
+  top: 0;
+  bottom: 0;
+  ${({ borderSide }) => borderSide}: 0px;
 
   overflow: hidden;
 
-  a {
+  .hoverLayer, a {
 
     font-size: 1.2vw;
     font-size: clamp(1.6rem, 1.2vw, 6rem);
     font-family: 'Montserrat',  sans-serif;
-    font-weight: 400;
     text-decoration: none;
 
     display: flex;
@@ -23,130 +21,51 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
 
-    min-height: 100vh;
-    height: 100vh;
-    min-width: 8rem;
-    width:  8vw;
-    width: clamp(8rem, 8vw, 30rem);
+    height: 100%;
+    min-width: 12rem;
+    width:  12vw;
+    width:  clamp(12rem, 10vw, 30rem);
 
     padding: 1rem;
 
     z-index: 100;
 
-    span {
-
-      color: ${({ theme }) => theme.color.fadeWhite};
-      font-size: inherit;
-      font-family: inherit;
-      margin-top: 4vw;
-    }
-
     img {
 
-      width: 32%;
-      position: absolute;
-      top: calc(50% - 5%);
+      width: 30%;
     }
 
-    &::after {
+    span {
 
-      content: '';
+      color: ${({ theme }) => theme.color.gray.bright};
+      font-size: inherit;
+      font-family: inherit;
 
-      background: url(${arrow}) center no-repeat ${({ theme }) => theme.color.brightBlack};
-      background-size: 32%;
-      background-position:center calc(50% - 3%);
-      color: ${({ theme }) => theme.color.fadeWhite};
-
-      display: flex;
-      align-items: center;
-      justify-content:center;
-
-      position: absolute;
-      top: 0;
-      bottom: 0;
-
-      width: 8rem;
-      width:100%;
-
-      transition: margin .2s ease-in;
-
-      padding-top: 4vw;
-
-      z-index: 5;
+      margin-top: 2vw;
     }
+  }
 
-    &.from-right {
+  .hoverLayer {
 
-      right: 0;
-      height: 100vh;
+    background: ${({ theme }) => theme.color.cold.blue};
+    border-radius: ${({ borderSide }) => (borderSide === 'left' && '0 1.8vw 1.8vw 0')
+    || (borderSide === 'right' && '1.8vw 0 0 1.8vw')};
+    pointer-events: none;
 
-      &::after {
+    position: absolute;
+    top:0;
+    bottom: 0;
+    ${({ borderSide }) => borderSide}: -13vw;
 
-        border-radius: 1vw 0 0 1vw;
-        margin-right: -200%;
-      }
+    transition:  ${({ borderSide }) => borderSide} .15s cubic-bezier(0.4, 0, 0.2, 1);
+  }
 
-      &:hover {
+  &:hover {
 
-          &::after {
+   .hoverLayer {
 
-            margin-right:0;
-        }
-      }
-    }
-    &.from-left {
-
-      left: 0;
-      height: 100vh;
-
-      .arrow {
-
-        transform: rotate(180deg)
-      }
-
-      &::after {
-
-        background: url(${leftArrow}) center no-repeat ${({ theme }) => theme.color.brightBlack};
-        background-size: 32%;
-        background-position:center calc(50% - 3%);
-        border-radius: 0 1vw 1vw 0;
-
-        margin-left: -200% ;
-      }
-
-      &:hover {
-
-        &::after {
-
-          margin-left:0;
-        }
-      }
-    }
-
-    &.to-portfolio {
-
-      &::after {
-        content: 'Portfolio';
-      }
-    }
-    &.to-home {
-
-      &::after {
-        content: 'Home';
-      }
-    }
-    &.to-skills {
-
-      &::after {
-        content: 'Skills';
-      }
-    }
-    &.to-contact {
-
-      &::after {
-        content: 'Contact';
-      }
-    }
+    ${({ borderSide }) => borderSide}: 0px;
+   }
   }
 `;
 
