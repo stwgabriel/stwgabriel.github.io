719e11b8368304562b54ff433f935953282c32a9
stwgabriel <gabrielstw@pm.me>
2022-01-02 16:10:05 -0300

    feat: add a min-height and update margins and paddings sizes

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 const PageView = styled.div`
 
   flex: 1;
+  min-height: 100vh;
 
-  padding: clamp(10rem, 10vh, 20rem) clamp(6rem, 3vw, 10rem);
-  margin-right: ${({ marginRight }) => (marginRight ? 'clamp(8rem, 8vw, 30rem)' : 0)};
+  padding: clamp(12rem, 12vh, 20rem) clamp(5rem, 5vw, 10rem);
   margin-right: ${({ marginRight }) => (marginRight ? '8vw' : 0)};
-  margin-left: ${({ marginLeft }) => (marginLeft ? 'clamp(8rem, 8vw, 30rem)' : 0)};
+  margin-right: ${({ marginRight }) => (marginRight ? 'clamp(12rem, 8vw, 30rem)' : 0)};
   margin-left: ${({ marginLeft }) => (marginLeft ? '8vw' : 0)};
+  margin-left: ${({ marginLeft }) => (marginLeft ? 'clamp(12rem, 8vw, 30rem)' : 0)};
 
 @media (min-width: 280rem) {
 
