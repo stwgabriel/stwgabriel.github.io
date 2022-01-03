3a5bf33ab2e4f3e117183dbd9f19db3a328b928d
stwgabriel <gabrielstw@pm.me>
2022-01-03 13:40:58 -0300

    fix: marginRight and marginLeft sizes on pageView

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -6,10 +6,10 @@ const PageView = styled.div`
   min-height: 100vh;
 
   padding: clamp(12rem, 12vh, 20rem) clamp(5rem, 5vw, 10rem);
-  margin-right: ${({ marginRight }) => (marginRight ? '8vw' : 0)};
-  margin-right: ${({ marginRight }) => (marginRight ? 'clamp(12rem, 8vw, 30rem)' : 0)};
-  margin-left: ${({ marginLeft }) => (marginLeft ? '8vw' : 0)};
-  margin-left: ${({ marginLeft }) => (marginLeft ? 'clamp(12rem, 8vw, 30rem)' : 0)};
+  margin-right: ${({ marginRight }) => (marginRight ? '10vw' : 0)};
+  margin-right: ${({ marginRight }) => (marginRight ? 'clamp(12rem, 10vw, 30rem)' : 0)};
+  margin-left: ${({ marginLeft }) => (marginLeft ? '10vw' : 0)};
+  margin-left: ${({ marginLeft }) => (marginLeft ? 'clamp(12rem, 10vw, 30rem)' : 0)};
 
 @media (min-width: 280rem) {
 
