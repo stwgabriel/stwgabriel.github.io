b67598f5300aad5d1a756dfb44703e6451691cc3
Caio <whoiscaio@protonmail.com>
2022-01-06 15:12:52 -0300

    feat: socials icon added

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -9,6 +9,10 @@ import PageTitle from '../../components/PageTitle';
 import { ContactContainer } from './styles';
 import Form from './components/Form';
 
+import linkedinIcon from '../../assets/images/icons/linkedin.svg';
+import githubIcon from '../../assets/images/icons/github.svg';
+import instagramIcon from '../../assets/images/icons/instagram.svg';
+
 function Contact() {
 
   const navigation = useContext(NavigationContext);
@@ -30,13 +34,13 @@ function Contact() {
         <ContactContainer>
           <div className="socials">
             <a href="https://linkedin.com" className="linkedin" target="_blank" rel="noreferrer">
-              Linkedin
+              <img src={linkedinIcon} alt="Linkedin Icon" />
             </a>
             <a href="https://github.com" className="github" target="_blank" rel="noreferrer">
-              Github
+              <img src={githubIcon} alt="Github Icon" />
             </a>
             <a href="https://instagram.com" className="instagram" target="_blank" rel="noreferrer">
-              Instagram
+              <img src={instagramIcon} alt="Instagram Icon" />
             </a>
           </div>
           <span>or</span>
