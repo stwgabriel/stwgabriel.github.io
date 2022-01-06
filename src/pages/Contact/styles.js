e28c828a37bac89eb6ead72378f100d432dd00ea
Caio <whoiscaio@protonmail.com>
2022-01-06 15:24:31 -0300

    feat: socials styles completed and span margin added

--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,6 +1,44 @@
 import styled from 'styled-components';
 
-const ContactContainer = styled.div``;
+const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 3vw;
+
+  .socials {
+    display: flex;
+
+    a {
+      border-radius: ${({ theme }) => theme.metric.borderRadius};
+      
+      display: flex;
+      align-items: center;
+
+      padding: .8vw;
+
+      &:first-child {
+        background: ${({ theme }) => theme.color.social.linkedin};
+      }
+
+      &:nth-child(2) {
+        background: ${({ theme }) => theme.color.social.github};
+        margin: 0 1.4vw;
+      }
+
+      &:last-child {
+        background: ${({ theme }) => theme.color.social.instagram};
+      }
+    }
+  }
+
+  span {
+    font-size: 1.2vw;
+    
+    margin: 2vw 0;
+  }
+`;
 
 const FormContainer = styled.form``;
 
