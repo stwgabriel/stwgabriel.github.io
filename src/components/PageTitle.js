dd11d24401c0375794612fac0d2046be88b1e70a
stwgabriel <gabrielstw@pm.me>
2022-01-08 08:24:32 -0300

    feat: update stroke size

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -6,7 +6,7 @@ const PageTitle = styled.h2`
   font-size: clamp(5rem, 4vw, 20rem);
   font-family: "ProDisplay", -apple-system, BlinkMacSystemFont, Tahoma, Arial Black,Verdana, Helvetica, Montserrat, sans-seif;
   font-weight:800;
-  -webkit-text-stroke: .15rem ${({ theme }) => theme.color.main.textColor};
+  -webkit-text-stroke: .12vw ${({ theme }) => theme.color.main.textColor};
 `;
 
 export default PageTitle;
