ce83012a0361f75cb26ef9ba0f7817f8c4c518c3
stwgabriel <gabrielstw@pm.me>
2022-01-08 08:40:17 -0300

    feat: create smooth scroll util

---
+++ src/utils/smoothScroll.js
@@ -0,0 +1,27 @@
+function handleScroll(e) {
+
+  e.preventDefault();
+  const { target } = e;
+  const finalTargetId = target.getAttribute('href');
+  const finalTarget = document.querySelector(finalTargetId).offsetTop;
+
+  document.getElementById('page-container').scroll({
+    top: finalTarget + 90,
+    behavior: 'smooth',
+  });
+}
+
+function addScrollListener() {
+
+  const intenalLinks = document.querySelectorAll('a[href^="#"]');
+
+  intenalLinks.forEach((item) => { item.addEventListener('click', handleScroll); });
+}
+
+function removeScrollListener() {
+
+  const intenalLinks = document.querySelectorAll('a[href^="#"]');
+  intenalLinks.forEach((item) => { item.removeEventListener('click', handleScroll); });
+}
+
+export { addScrollListener, removeScrollListener };
