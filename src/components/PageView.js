ea21149f1e244f9ad61a8e9c5e1e42a695274657
stwgabriel <gabrielstw@pm.me>
2022-01-08 14:17:28 -0300

    feat: reduce borders white space
    prepare view to mobile version

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -9,17 +9,22 @@ const PageView = styled.div`
 
   overflow: auto;
 
-  padding: clamp(12rem, 12vh, 20rem) clamp(5rem, 5vw, 10rem);
+  padding: clamp(8rem, 10vh, 20rem) clamp(5rem, 5vw, 10rem);
   margin-right: ${({ marginRight }) => (marginRight ? '10vw' : 0)};
   margin-right: ${({ marginRight }) => (marginRight ? 'clamp(12rem, 10vw, 30rem)' : 0)};
   margin-left: ${({ marginLeft }) => (marginLeft ? '10vw' : 0)};
   margin-left: ${({ marginLeft }) => (marginLeft ? 'clamp(12rem, 10vw, 30rem)' : 0)};
 
-@media (min-width: 280rem) {
+  @media (max-width: 999px) {
 
-  max-width: 300rem;
-  margin: 0 auto;
-}
+    margin: 0 !important;
+  }
+
+  @media (min-width: 280rem) {
+
+    max-width: 300rem;
+    margin: 0 auto;
+  }
 `;
 
 export default PageView;
