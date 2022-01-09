96815d621422836f62c0d200a316b756696ef4f9
stwgabriel <gabrielstw@pm.me>
2022-01-09 15:08:12 -0300

    feat: add smooth scroll and add NavMenu

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -1,18 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { NavigationContext } from '../../contexts/NavigationContext';
+
+import { addScrollListener, removeScrollListener } from '../../utils/smoothScroll';
 
 import PageContainer from '../../components/PageContainer';
 import PageBorder from '../../components/PageBorder';
 import PageView from '../../components/PageView';
 import PageTitle from '../../components/PageTitle';
+import NavMenu from '../../components/NavMenu';
 
 function Portfolio() {
   const navigation = useContext(NavigationContext);
   const { handleNavigation } = navigation;
 
+  useEffect(() => {
+
+    addScrollListener();
+
+    return () => {
+
+      removeScrollListener();
+    };
+  });
+
   return (
-    <PageContainer>
+    <PageContainer id="page-container">
       <PageBorder
         borderSide="left"
         handleNavigation={handleNavigation}
@@ -22,6 +35,10 @@ function Portfolio() {
         Portfolio
       </PageBorder>
       <PageView>
+        <NavMenu />
+        <button type="button" id="main-content">
+          {' '}
+        </button>
         <PageTitle className="no-select">Skills</PageTitle>
       </PageView>
       <PageBorder
