0a62187e282c292192f5175febdeaca7ca2933e8
stwgabriel <gabrielstw@pm.me>
2022-01-11 14:12:30 -0300

    feat: add mobile navigation

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -5,16 +5,17 @@ import { NavigationContext } from '../../contexts/NavigationContext';
 import { addScrollListener, removeScrollListener } from '../../utils/smoothScroll';
 
 import PageContainer from '../../components/PageContainer';
-import PageBorder from '../../components/PageBorder';
 import PageView from '../../components/PageView';
+import PageBorderNav from '../../components/PageBorderNav';
+import NavMenu from '../../components/NavMenu';
 import PageTitle from '../../components/PageTitle';
-import { ContactContainer } from './styles';
 import Form from './components/Form';
+
+import { ContactContainer } from './styles';
 
 import linkedinIcon from '../../assets/images/icons/linkedin.svg';
 import githubIcon from '../../assets/images/icons/github.svg';
 import instagramIcon from '../../assets/images/icons/instagram.svg';
-import NavMenu from '../../components/NavMenu';
 
 function Contact() {
 
@@ -34,14 +35,14 @@ function Contact() {
   return (
 
     <PageContainer id="page-container">
-      <PageBorder
+      <PageBorderNav
         borderSide="left"
         handleNavigation={handleNavigation}
         from="from-left"
         to="skills"
       >
         Skills
-      </PageBorder>
+      </PageBorderNav>
       <PageView marginRight>
         <NavMenu />
         <button type="button" id="main-content">
