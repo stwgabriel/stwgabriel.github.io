7da4a1acc137038a38fb74cabdbb3dbe251c3a40
stwgabriel <gabrielstw@pm.me>
2022-01-17 19:06:07 -0300

    feat: create skills page

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -10,22 +10,57 @@ import PageBorderNav from '../../components/PageBorderNav';
 import NavMenu from '../../components/NavMenu';
 import PageTitle from '../../components/PageTitle';
 
-function Portfolio() {
+import reactIcon from '../../assets/images/skills/react-icon.svg';
+import javascriptIcon from '../../assets/images/skills/javascript-icon.svg';
+import sassIcon from '../../assets/images/skills/sass-icon.svg';
+import cssIcon from '../../assets/images/skills/css-icon.svg';
+import htmlIcon from '../../assets/images/skills/html-icon.svg';
+import gitIcon from '../../assets/images/skills/git-icon.svg';
+import figmaIcon from '../../assets/images/skills/figma-icon.svg';
+
+import typescriptIcon from '../../assets/images/skills/typescript-icon.svg';
+import nodejsIcon from '../../assets/images/skills/nodejs-icon.svg';
+
+import reduxIcon from '../../assets/images/skills/redux-icon.svg';
+import nextjsIcon from '../../assets/images/skills/nextjs-icon.svg';
+import swiftIcon from '../../assets/images/skills/swift-icon.svg';
+import reactNativeIcon from '../../assets/images/skills/react-native-icon.svg';
+
+import { SkillsPageView, SkillsContainer, XpContainer } from './styles';
+
+function Skills() {
+
   const navigation = useContext(NavigationContext);
   const { handleNavigation } = navigation;
 
-  useEffect(() => {
+  const mainSkills = [
 
-    addScrollListener();
+    { imageUrl: reactIcon, name: 'React' },
+    { imageUrl: javascriptIcon, name: 'Javascript' },
+    { imageUrl: sassIcon, name: 'Sass' },
+    { imageUrl: cssIcon, name: 'CSS' },
+    { imageUrl: htmlIcon, name: 'HTML' },
+    { imageUrl: gitIcon, name: 'Git' },
+    { imageUrl: figmaIcon, name: 'Figma' },
+  ];
+  const learningSkills = [
 
-    return () => {
+    { imageUrl: typescriptIcon, name: 'Typescript' },
+    { imageUrl: nodejsIcon, name: 'NodeJs' },
+  ];
+  const intendToLearnSkills = [
+    { imageUrl: reduxIcon, name: 'Redux' },
+    { imageUrl: nextjsIcon, name: 'NextJs' },
+    { imageUrl: swiftIcon, name: 'Swift' },
+    { imageUrl: reactNativeIcon, name: 'React Native' },
+  ];
 
-      removeScrollListener();
-    };
-  });
+  useEffect(() => { addScrollListener(); return () => { removeScrollListener(); }; });
 
   return (
+
     <PageContainer id="page-container">
+
       <PageBorderNav
         borderSide="left"
         handleNavigation={handleNavigation}
@@ -34,13 +69,81 @@ function Portfolio() {
       >
         Portfolio
       </PageBorderNav>
+
       <PageView>
+
         <NavMenu />
-        <button type="button" id="main-content">
+
+        <button type="button" id="main-content" aria-hidden="true">
           {' '}
         </button>
-        <PageTitle className="no-select">Skills</PageTitle>
+
+        <PageTitle className="no-select">Skills &amp; Xp</PageTitle>
+
+        <SkillsPageView>
+
+          <SkillsContainer>
+
+            <h2>Main Skills</h2>
+            <div className="skillsRow">
+              {mainSkills.map((skill) => (
+
+                <code>
+                  <img
+                    title={skill.name}
+                    src={skill.imageUrl}
+                    alt={`${skill.name} icon`}
+                  />
+                </code>
+              ))}
+            </div>
+
+            <h2>Currently learning</h2>
+            <div className="skillsRow">
+              {learningSkills.map((skill) => (
+
+                <code>
+                  <img
+                    title={skill.name}
+                    src={skill.imageUrl}
+                    alt={`${skill.name} icon`}
+                  />
+                </code>
+              ))}
+            </div>
+
+            <h2>I&lsquo;ll learn</h2>
+            <div className="skillsRow">
+              {intendToLearnSkills.map((skill) => (
+
+                <code>
+                  <img
+                    title={skill.name}
+                    src={skill.imageUrl}
+                    alt={`${skill.name} icon`}
+                  />
+                </code>
+              ))}
+            </div>
+          </SkillsContainer>
+
+          <XpContainer>
+            <h2>My Xp</h2>
+            <p>
+              Since I started my journey as a Front-end student almost 3 years ago,
+              I have learned a lot about and done some good personal projects and now
+              I am looking for an opportunity to use what I have learned and learn
+              more with a good team.
+              <br />
+              <br />
+              All my projects are available on my Github profile and if you think
+              I can be part of your team and we can team up you can
+              contact me on the contact page.
+            </p>
+          </XpContainer>
+        </SkillsPageView>
       </PageView>
+
       <PageBorderNav
         borderSide="right"
         handleNavigation={handleNavigation}
@@ -53,4 +156,4 @@ function Portfolio() {
   );
 }
 
-export default Portfolio;
+export default Skills;
--- src/pages/Skills/styles.js
+++ src/pages/Skills/styles.js
@@ -1,8 +1,129 @@
 import styled from 'styled-components';
 
-const Content = styled.div`
+const SkillsPageView = styled.div`
+
+  display: flex;
 
   width: 100%;
+  min-height: fit-content;
+
+  @media (max-width: 1150px) {
+
+    display: grid;
+    grid-gap: 6rem;
+    grid-template-areas: 'skills' 'xp';
+  }
 `;
 
-export { Content };
+const SkillsContainer = styled.section`
+
+  grid-area: skills;
+  flex: 1;
+
+  height: fit-content;
+
+  padding-right: 2vw;
+  margin-top : -2vw;
+
+  h2 {
+
+    font-size: 1.6vw;
+    font-weight: 700;
+
+    margin-top: 3vw;
+  }
+
+  code {
+
+    background: ${({ theme }) => theme.color.main.selection};
+    border-radius: ${({ theme }) => theme.metric.borderRadius};
+
+    padding: .15vw .3vw;
+    margin-right: .5vw;
+
+    img {
+
+      background: ${({ theme }) => theme.color.main.background};
+      border-radius: ${({ theme }) => theme.metric.borderRadius};
+
+      width: 4vw;
+
+      padding: .1vw;
+      margin-top: 1vw;
+      pointer-events: all;
+    }
+  }
+
+  @media (max-width: 1150px) {
+
+    margin-top : -3rem;
+
+    h2 {
+
+      font-size: 2.4rem;
+
+      margin-top: 3rem;
+    }
+
+    code {
+
+      border-radius: ${({ theme }) => theme.metric.mobileBorderRadius};
+
+      padding: .15rem .3rem;
+      margin-right: .8rem;
+
+      img {
+
+        border-radius: ${({ theme }) => theme.metric.mobileBorderRadius};
+
+        width: 5.8rem;
+
+        padding: .3rem;
+        margin-top: 2rem;
+      }
+    }
+  }
+`;
+
+const XpContainer = styled.section`
+
+  font-size: 1.5vw;
+  font-size: clamp(1.8rem, 1.5vw, 6.5rem);
+
+  grid-area: xp;
+  flex: 1;
+
+  height: fit-content;
+
+  margin-top: 1vw;
+
+  h2 {
+
+    font-size: 1.6vw;
+    font-weight: 700;
+
+    margin-bottom: .8vw;
+  }
+
+  p {
+
+    font-size: 1.9rem;
+    font-size: clamp(1.9rem, 1.3vw, 6.5rem);
+    font-weight: 500;
+    line-height: 2.3ch;
+    margin: 1vw 0;
+  }
+
+  @media (max-width: 1150px) {
+
+    h2 {
+
+      font-size: 2.4rem;
+
+      margin-bottom: 2rem;
+    }
+
+  }
+`;
+
+export { SkillsPageView, SkillsContainer, XpContainer };
