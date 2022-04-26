9c9f7d64192012d62d2e112d01258799de920ea6
stwgabriel <gabrielstw@pm.me>
2022-04-26 15:20:31 -0300

    chore: covert into next base app

--- src/utils/isEmailValid.js
+++
@@ -1,8 +0,0 @@
-function isEmailValid(value) {
-
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return regex.test(value);
-}
-
-export default isEmailValid;
