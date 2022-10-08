ac0cf8ec781220852d5b534f82926916d7763719
stwgabriel <talktogabriel@pm.me>
2022-10-08 15:48:20 -0300

    chore: under maintenance

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-expressions */
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 import { v4 as uuid } from 'uuid';
 
+import { Link, useHistory } from 'react-router-dom';
 import { Overlay, Container } from './styles';
 
 import closeButton from '../../assets/images/icons/close.svg';
@@ -11,19 +13,21 @@ import clickToExit from '../../utils/clickToExit';
 
 function Modal({
   renderModal, handleRenderModal,
-  title, message, subMessage, links,
+  title, message, subMessage, links, unCloseable,
 }) {
 
   if (!renderModal) { return null; }
 
   const ExitClassName = `click-to-exit_${uuid()}`;
 
+  const history = useHistory();
+
   function handleClick(element) {
 
     const { target } = element;
 
     if (clickToExit(target, ExitClassName)) {
-      handleRenderModal();
+      !unCloseable ? handleRenderModal() : (() => '')();
     }
   }
 
@@ -39,12 +43,21 @@ function Modal({
       <Container>
 
         <header>
-
           <h2>{title}</h2>
+          {
+            !unCloseable
+              ? (
+                <button
+                  type="button"
+                  id="close-button"
+                  onClick={handleRenderModal}
+                >
+                  <img src={closeButton} alt="close" />
+                </button>
+              )
+              : ''
+          }
 
-          <button type="button" id="close-button" onClick={handleRenderModal}>
-            <img src={closeButton} alt="close" />
-          </button>
         </header>
 
         <main>
@@ -64,6 +77,49 @@ function Modal({
             </a>
           ))}
         </footer>
+
+        {
+          (unCloseable && history.length > 3) ? (
+            <button
+              type="button"
+              onClick={() => history.goBack()}
+              style={{
+                background: '#454158',
+                borderRadius: '1rem',
+                color: '#fff',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+                display: 'block',
+                padding: '1rem',
+                margin: '2rem auto 0',
+                textAlign: 'center',
+                width: '12rem',
+              }}
+            >
+              Go Back
+            </button>
+          ) : (
+            <Link
+              style={{
+                background: '#454158',
+                borderRadius: '1rem',
+                color: '#fff',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+                display: 'block',
+                padding: '1rem',
+                margin: '2rem auto 0',
+                textAlign: 'center',
+                width: '12rem',
+              }}
+              to="/"
+              className="onLeft mobile-control from-left"
+            >
+              Go Home
+            </Link>
+          )
+      }
+
       </Container>
     </Overlay>,
     document.getElementById('modal-root'),
