6d95b5071789928b85013e976e597eb50adbf7f3
stwgabriel <contact@stwgabriel.com>
2023-05-05 15:39:29 -0300

    chore: update styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,15 @@
-import 'src/styles/index.css'
-
 import { Analytics } from '@vercel/analytics/react'
 import { AppProps } from 'next/app'
-import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
+import { Inter } from 'next/font/google'
 //
 import RootProvider from 'src/providers'
 import AppStyles from 'src/styles/App'
 import GlobalStyles from 'src/styles/global'
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
+const inter = Inter({
   variable: '--font-sans',
   subsets: ['latin'],
   weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
 })
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -33,18 +18,14 @@ export default function App({ Component, pageProps }: AppProps) {
       {/* eslint-disable-next-line react/no-unknown-property */}
       <style jsx global>
         {`
-          :root {
-            --font-mono: ${mono.style.fontFamily};
-            --font-sans: ${sans.style.fontFamily};
-            --font-serif: ${serif.style.fontFamily};
+          * {
+            font-family: ${inter.style.fontFamily};
           }
         `}
       </style>
       <RootProvider>
         <GlobalStyles />
-        <AppStyles>
-          <Component {...pageProps} />
-        </AppStyles>
+        <Component {...pageProps} />
         <div id="outer-view" />
         <Analytics />
       </RootProvider>
